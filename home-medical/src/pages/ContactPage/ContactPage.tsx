117add8fb514146fa22c68917499c5a048b989a9
Emma-Lorincz <Emma-Lorincz@users.noreply.github.com>
2021-04-19 10:32:43 +0300

    Fb messenger updated.

--- home-medical/src/pages/ContactPage/ContactPage.tsx
+++ home-medical/src/pages/ContactPage/ContactPage.tsx
@@ -5,31 +5,11 @@ import { FacebookProvider, Page } from "react-facebook";
 import { Link } from "react-router-dom";
 
 class ContactPage extends React.Component {
-	componentDidMount() {
-		console.log("mount");
-	}
+	componentDidMount() {}
 
 	componentWillUnmount() {}
 
 	render() {
-		let fbPageAttributes = {
-			"data-href": "https://www.facebook.com/homemedicalservsrl/",
-			"data-tabs": "messages",
-			"data-width": "",
-			"data-height": "",
-			"data-small-header": "false",
-			"data-adapt-container-width": "true",
-			"data-hide-cover": "false",
-			"data-show-facepile": "true",
-		};
-
-		let scriptProperties = {
-			crossorigin: "anonymous",
-			nonce: "jqGVewxi",
-		};
-
-		console.log("render");
-
 		return (
 			<div className={`${styles.ContactPage} simple-content`}>
 				<ul className="icofont-ul">
@@ -48,20 +28,11 @@ class ContactPage extends React.Component {
 							@homemedicalservsrl
 						</a>
 					</li>
+					<li>
+						<span className="icofont-location-pin"></span> Brăila, jud. Brăila
+					</li>
 				</ul>
 				<div id="fb-root"></div>
-				<Script
-					url={`https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&_rand=${Math.random()}`}
-					attributes={scriptProperties}></Script>
-				<div className={styles.fbPage} {...fbPageAttributes}>
-					<blockquote
-						cite="https://www.facebook.com/homemedicalservsrl/"
-						className="fb-xfbml-parse-ignore">
-						<a href="https://www.facebook.com/homemedicalservsrl/">
-							Home Medical Serv SRL
-						</a>
-					</blockquote>
-				</div>
 
 				<div className={styles.fbPage}>
 					<FacebookProvider appId="868929373664504">
